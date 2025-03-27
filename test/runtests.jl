9b4d64db2ed51c8be11a947577dd015bf4544a57
YanisLeFur <64954297+YanisLeFur@users.noreply.github.com>
2025-03-27 14:34:42 +0100

    testing num_tls

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,9 @@ end
 @testset "num_dicke_ladders" begin
     @test [num_dicke_ladders(N) for N in 1:9] == [1, 2, 2, 3, 3, 4, 4, 5, 5]
 end
+
+@testset "num_tls" begin
+    N_dicke = [2, 4, 6, 9, 12, 16, 30, 36, 121, 2601, 3906]
+    @test [num_tls(i) for i in N_dicke] == [1, 2, 3, 4, 5, 6, 9, 10, 20, 100, 123]
+
+end
