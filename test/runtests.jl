a7a7e9882fadfc2a44e7ac18deec408314f0991c
YanisLeFur <64954297+YanisLeFur@users.noreply.github.com>
2025-03-27 15:10:59 +0100

    testing isdiagonal

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Piqs
 using Test
+using QuantumToolbox
 
 @testset "num_dicke_states" begin
     @test num_dicke_states(2) == 4
@@ -16,5 +17,21 @@ end
 @testset "num_tls" begin
     N_dicke = [2, 4, 6, 9, 12, 16, 30, 36, 121, 2601, 3906]
     @test [num_tls(i) for i in N_dicke] == [1, 2, 3, 4, 5, 6, 9, 10, 20, 100, 123]
+
+end
+
+@testset "isdiagonal" begin
+    a1 = Array([[1, 2], [3, 4]])
+    mat1 = reshape(collect(Iterators.flatten(a1)), (length(a1[1]), length(a1)))
+    mat2 = qeye(2)
+    a3 = Array([[1 + 1im, 0.0], [0.0, 2 - 2im]])
+    mat3 = reshape(collect(Iterators.flatten(a3)), (length(a3[1]), length(a3)))
+    mat4 = reshape(collect(1:2:18), (3, 3))
+
+    @test isdiagonal(mat1) == false
+    @test isdiagonal(mat2) == true
+    @test isdiagonal(mat3) == true
+    @test isdiagonal(mat4) == false
+
 
 end
