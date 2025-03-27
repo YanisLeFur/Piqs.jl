e2421bdd8c01488a9a501e68894fe53faf52aebc
YanisLeFur <64954297+YanisLeFur@users.noreply.github.com>
2025-03-27 14:25:40 +0100

    adding num_dicke_ladders

--- src/dicke.jl
+++ src/dicke.jl
@@ -1,16 +1,16 @@
-export num_dicke_states
+export num_dicke_states, num_dicke_ladders
 
 function num_dicke_states(N::Integer)::Integer
     """Calculate number of Dicke states
 
     Parameters
     ----------
-    N: unsigned int
+    N: Integer
         The number of two-level systems.
 
     Returns
     -------
-    nds: unsigned int
+    nds: Integer
         The number of Dicke states.
     """
     if (N < 1)
@@ -19,4 +19,29 @@ function num_dicke_states(N::Integer)::Integer
     return (N / 2 + 1)^2 - (N % 2) / 4
 
 end
+
+
+function num_dicke_ladders(N::Integer)::Integer
+    """Calculate the total number of ladders in the Dicke space.
+
+    For a collection of N two-level systems it counts how many different
+    "j" exist or the number of blocks in the block-diagonal matrix.
+
+    Parameters
+    ----------
+    N: Integer
+        The number of two-level systems.
+
+    Returns
+    -------
+    Nj: Integer
+        The number of Dicke ladders.
+    """
+
+    return (N + 1) * 0.5 + (1 - mod(N, 2)) * 0.5
+end
+
+
+
+
 
