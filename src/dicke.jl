78636393d3aa5ffea9d14402a192aa140cdc9bb3
YanisLeFur <64954297+YanisLeFur@users.noreply.github.com>
2025-03-27 12:19:03 +0100

    num_dicke_state creation

--- src/dicke.jl
+++ src/dicke.jl
@@ -1,5 +1,22 @@
-module Piqs
+export num_dicke_states
 
-# Write your package code here.
+function num_dicke_states(N::Integer)::Integer
+    """Calculate number of Dicke states
+
+    Parameters
+    ----------
+    N: unsigned int
+        The number of two-level systems.
+
+    Returns
+    -------
+    nds: unsigned int
+        The number of Dicke states.
+    """
+    if (N < 1)
+        throw(DomainError(N, "The number of states must be greater than one."))
+    end
+    return (N / 2 + 1)^2 - (N % 2) / 4
 
 end
+
