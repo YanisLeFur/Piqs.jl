0767d09235bc5dc54ce757259177dd2613169180
YanisLeFur <64954297+YanisLeFur@users.noreply.github.com>
2025-03-27 14:25:50 +0100

    testing num_dicke_ladders

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,14 @@
 using Piqs
 using Test
 
-@testset "Dicke.jl" begin
+@testset "num_dicke_states" begin
     @test num_dicke_states(2) == 4
     @test num_dicke_states(143) == 5256
 
     @test_throws DomainError num_dicke_states(0)
     @test_throws DomainError num_dicke_states(-1)
+end
+
+@testset "num_dicke_ladders" begin
+    @test [num_dicke_ladders(N) for N in 1:9] == [1, 2, 2, 3, 3, 4, 4, 5, 5]
 end
