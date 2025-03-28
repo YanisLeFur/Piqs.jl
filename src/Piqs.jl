9d529622e033f97884675d95fd7711b0edb399a3
YanisLeFur <64954297+YanisLeFur@users.noreply.github.com>
2025-03-28 17:33:12 +0100

    add lindbladian

--- src/Piqs.jl
+++ src/Piqs.jl
@@ -2,6 +2,5 @@ module Piqs
 import QuantumToolbox
 include("dicke.jl")
 include("loss_factors.jl")
-# include("operators.jl")
-
+include("operators.jl")
 end
---
+++ src/operators.jl
@@ -0,0 +1,50 @@
+export lindbladian
+
+
+function lindbladian(d::Dicke)::QuantumObject
+    """
+    Build the Lindbladian superoperator of the dissipative dynamics as a
+    sparse matrix.
+
+    Returns
+    ----------
+    lindblad_qobj: QuantumObject
+        The matrix size is (nds**2, nds**2) where nds is the number of
+        Dicke states.
+    """
+    N = d.N
+    nds = num_dicke_states(N)
+
+    lindblad_row = []
+    lindblad_col = []
+    lindblad_data = []
+
+    _1, _2, jmm1_row, jmm1_inv = jmm1_dictionary(N)
+
+    for r in keys(jmm1_row)
+        j, m, m1 = jmm1_row[r]
+        jmm1 = [(j, m, m1),
+            (j, m + 1, m1 + 1),
+            (j + 1, m + 1, m1 + 1),
+            (j - 1, m + 1, m1 + 1),
+            (j + 1, m, m1),
+            (j - 1, m, m1),
+            (j + 1, m - 1, m1 - 1),
+            (j, m - 1, m1 - 1),
+            (j - 1, m - 1, m1 - 1)]
+        gamma_functions = [gamma1, gamma2, gamma3, gamma4, gamma5, gamma6, gamma7, gamma8, gamma9]
+        for i in eachindex(gamma_functions)
+            if jmm1[i] in keys(jmm1_inv)
+                g = gamma_functions[i](d, jmm1[i])
+                c = jmm1_inv[jmm1[i]]
+
+                push!(lindblad_row, r + 1)
+                push!(lindblad_col, c + 1)
+                push!(lindblad_data, g)
+            end
+        end
+    end
+    lindblad_matrix = sparse((lindblad_row), (lindblad_col), Float64.(lindblad_data), nds^2, nds^2)
+    lindblad_qobj = Qobj(lindblad_matrix, dims=(nds, nds))
+    return lindblad_qobj
+end
